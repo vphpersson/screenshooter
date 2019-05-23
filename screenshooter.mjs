bece87441660807d794e087e84c9803ca7e301d2
vph <vph@giskard>
2019-05-23 13:14:08 +0200

    Added color logic.

--- screenshooter.mjs
+++ screenshooter.mjs
@@ -5,6 +5,41 @@ import process from 'process';
 
 import puppeteer from 'puppeteer';
 import ArgumentParser from 'argparse';
+import sharp from 'sharp';
+import get_pixels from 'get-pixels';
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function get_filename(input_url, ext) {
+    const url = new URL(input_url);
+    const port = url.port
+        ? url.port
+        : (() => {
+            switch (url.protocol) {
+                case 'http:':
+                    return '80';
+                case 'https:':
+                    return '443';
+            }
+        })()
+    ;
+
+    return `${url.host}_${url.protocol.replace(':', '')}_${port}.${ext}`;
+}
+
+async function get_num_colors(png_buffer) {
+    const pixels = await new Promise((resolve => {
+        get_pixels(png_buffer, 'image/png', (err, pixels) => {
+            if (err)
+                throw new Error(err);
+            resolve(pixels);
+        });
+    }));
+    return new Set(pixels.data).size;
+}
 
 async function take_screenshoot(browser, url) {
     const page = await browser.newPage();
@@ -13,25 +48,9 @@ async function take_screenshoot(browser, url) {
     try {
         const response = await page.goto(url, {timeout: 3800, waitUntil: 'load'});
 
-        const port = url.port
-            ? url.port
-            : (() => {
-                switch (url.protocol) {
-                    case 'http:':
-                        return '80';
-                    case 'https:':
-                        return '443';
-                }
-            })()
-        ;
-
-        // await page.screenshot({path: `./${url.host}_${url.protocol.replace(':', '')}_${port}.png`, type: 'png'});
-
-        const screenshot_data = await page.screenshot();
-
-        console.log(`\x1b[32m${url.toString()}\x1b[0m`);
-
         return {
+            url,
+            timestamp: Date.now(),
             request: {
                 headers: response._request._headers,
                 method: response._request._method,
@@ -45,12 +64,26 @@ async function take_screenshoot(browser, url) {
                 headers: response._headers,
                 status: response._status,
                 status_text: response._statusText,
-                response_data: await response.buffer()
+                response_data_b64: (await response.buffer()).toString('base64')
             },
-            screenshot_data: screenshot_data
+            screenshot_b64: (await (async () => {
+                const screenshot_buffer = await page.screenshot();
+                if (response.ok()) {
+                    const resized_greyscale_screenshot_buffer = await sharp(screenshot_buffer)
+                        .resize(9)
+                        .greyscale()
+                        .toBuffer()
+                    ;
+                    if (await get_num_colors(resized_greyscale_screenshot_buffer) < 15) {
+                        await sleep(5000);
+                        return page.screenshot();
+                    }
+                }
+                return screenshot_buffer;
+            })()).toString('base64')
         }
     } catch (err) {
-        throw Error(`\x1b[33m${url.toString()}: ${err.toString()}\x1b[0m`);
+        throw Error(`${url.toString()}: ${err.toString()}`);
     } finally {
         await page.close();
     }
@@ -93,8 +126,7 @@ function get_parser() {
     parser.addArgument(
         [ '-n', '--num-concurrent' ],
         {
-            help: 'Number of concurrent workers taking screenshots..',
-            nargs: ArgumentParser.Const.ONE_OR_MORE,
+            help: 'Number of concurrent workers taking screenshots.',
             metavar: 'N',
             dest: 'num_concurrent',
             type: 'int',
@@ -105,8 +137,10 @@ function get_parser() {
     return parser;
 }
 
-(async () => {
+async function main() {
     const args = get_parser().parseArgs();
+
+    process.chdir(args.output_dir);
 
     const url_stack = await (async () => {
         const url_set = new Set();
@@ -134,41 +168,54 @@ function get_parser() {
     })();
 
     const browser = await puppeteer.launch({headless: true, ignoreHTTPSErrors: true});
-    const results = [];
 
-    async function work() {
-        while (url_stack.length > 0) {
-            const url_str = url_stack.pop();
-            if (!url_str)
-                continue;
+    const num_urls = url_stack.length;
+    const num_urls_num_digits = String(num_urls).length;
+    let num_urls_requested = 0;
 
-            try {
-                results.push(await take_screenshoot(browser, new URL(url_str)));
-            } catch (err) {
-                console.error(err);
+    const results = await (async () => {
+        const results = [];
+
+        async function work() {
+
+            while (url_stack.length > 0) {
+                const url_str = url_stack.pop();
+                if (!url_str)
+                    continue;
+
+                const progress_str = `${String(++num_urls_requested).padStart(num_urls_num_digits, '0')}/${num_urls}`;
+
+                try {
+                    const take_screenshot_result = await take_screenshoot(browser, new URL(url_str));
+                    const destination_path = `./${get_filename(take_screenshot_result.url, 'png')}`;
+                    await fs.promises.writeFile(
+                        destination_path,
+                        Buffer.from(take_screenshot_result.screenshot_b64, 'base64')
+                    );
+                    console.log(`[${progress_str}] \x1b[32m${url_str}\x1b[0m`);
+                    results.push(take_screenshot_result);
+                } catch (err) {
+                    console.error(`[${progress_str}] \x1b[33m${err.message}\x1b[0m`);
+                }
             }
         }
-    }
 
-    process.chdir(args.output_dir);
-    await Promise.all(Array(args.num_concurrent).fill().map(() => work()));
+        await Promise.all(Array(args.num_concurrent).fill().map(() => work()));
 
-    console.log(JSON.stringify(results));
-
-    // for (let i = 0; i < url_arr.length; i += NUM_CONCURRENT) {
-    //     const promises = url_arr.slice(i, i + NUM_CONCURRENT).map(url_str => ));
-    //     let off = 0;
-    //     for (const promise of promises) {
-    //         console.log(`[${i+off+1}/${url_arr.length}] Trying ${url_arr[i+off]}...`);
-    //         try {
-    //             await promise;
-    //         } catch (err) {
-    //             console.error(err);
-    //         }
-    //         off++;
-    //     }
-    // }
+        return results;
+    })();
 
     await browser.close();
-})();
 
+    // console.log(JSON.stringify(results));
+}
+
+main()
+    .then(() => {
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    })
+;
